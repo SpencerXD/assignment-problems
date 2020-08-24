99d81c99c764b124b2a577699df5263d5e80b106
Spencer xD <swtimmons@gmail.com>
2020-08-24 04:24:24 +0000

    linted code

--- is_symmetric.py
+++ is_symmetric.py
@@ -1,14 +1,14 @@
 
 def is_symmetric(input_string):
     if input_string == input_string[::-1]:
-        symmetry = True 
-    else: 
+        symmetry = True
+    else:
         symmetry = False
 
     return symmetry
 
 print("testing is_symmetric on input 'racecar'...")
-assert  is_symmetric("racecar") is True, "racecar Test Failed"
+assert is_symmetric("racecar") is True, "racecar Test Failed"
 print("PASSED")
 
 print("testing is_symmetric on input 'batman'...")

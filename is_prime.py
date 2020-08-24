a055dcfebda84af943698211c6ab6af27b2c91e3
Spencer xD <swtimmons@gmail.com>
2020-08-24 04:25:50 +0000

    linted code

--- is_prime.py
+++ is_prime.py
@@ -1,6 +1,6 @@
 def is_prime(input_value):
-    for m in range (2, input_value):
-        if input_value%m == 0:
+    for m in range(2, input_value):
+        if input_value % m == 0:
             check = False
     return True
 print("testing is_prime on input 59...")
